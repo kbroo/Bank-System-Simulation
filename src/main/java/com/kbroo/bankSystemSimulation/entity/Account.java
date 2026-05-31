9d4cfa75d3cd1e81e9ed086346920b43f8ecaaf1
Kbroo <kbroomyk@gmail.com>
2026-05-31 22:08:58 +0300

    Removed message with success result of deposit

--- src/main/java/com/kbroo/bankSystemSimulation/entity/Account.java
+++ src/main/java/com/kbroo/bankSystemSimulation/entity/Account.java
@@ -57,7 +57,6 @@ abstract public class Account {
             System.out.println("Отрицательная или равная нулю сумма пополнения.");
         } else {
             this.balance = this.balance.add(amount);
-            System.out.println("Счет пополнен.");
         }
     };
 
